67a5f360b4777f9e0aca75e6088ad2a52adb6f55
nurfce <82702789+nurfce@users.noreply.github.com>
2021-12-30 16:58:39 +0300

    hw 2 güncellendi

--- src/HW2.java
+++ src/HW2.java
@@ -3,8 +3,8 @@ import java.util.Scanner ;
 public class HW2 {
 
 	public static void main (String[]args) {
-//Eðer girilen tutar 0 ve 1000 TL arasýnda ise KDV oraný %18 , 
-//tutar 1000 TL'den büyük ise KDV oranýný %8 olarak KDV tutarý hesaplayan programý yazýnýz.
+// Eðer girilen tutar 0 ve 1000 TL arasýnda ise KDV oraný %18 , 
+// tutar 1000 TL'den büyük ise KDV oranýný %8 olarak KDV tutarý hesaplayan programý yazýnýz.
 
 		Scanner input= new Scanner(System.in); 
 		
@@ -19,7 +19,7 @@ public class HW2 {
 		System.out.println("KDV Oraný:"+ KDVoraný );
 		
 		KDVtutarý = KDVoraný*Tutar;
-		System.out.println("KDV Tutarý:" + KDVtutarý);
+		System.out.println("KDV Tutarý: " + KDVtutarý);
 		
 		ToplamTutar= Tutar+KDVtutarý ;
 		System.out.println("Toplam Ödemeniz: " + ToplamTutar);
