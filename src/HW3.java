6fa7c6f34d09e9a1049ce74a60cf0a0f9a66cca0
nurfce <82702789+nurfce@users.noreply.github.com>
2021-12-30 15:03:22 +0300

    3.ödev eklendi

--- src/HW3.java
+++ src/HW3.java
@@ -1,9 +1,34 @@
-
+import java.util.Scanner ;
 public class HW3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢
+       //𝑢 = (a+b+c) / 2
 
+      //Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)
+
+      Scanner input= new Scanner (System.in) ; 		
+		
+	double a,b,c,cevre,u,alan;
+	
+	System.out.println("Üçgenin 1.kenar uzunluğunu girin:");
+	a= input.nextDouble();
+	System.out.println("Üçgenin 2.kenar uzunluğunu girin:");
+	b= input.nextDouble();
+	System.out.println("Üçgenin 3.kenar uzunluğunu girin:");
+	c= input.nextDouble();	
+	
+	cevre= a+b+c;
+	u=cevre/2;
+	alan= Math.sqrt(u*(u-a)*(u-b)*(u-c)) ;
+	 System.out.println("Üçgenin Alanı: "+alan);
+	 
+		
+		
+		
+		
+		
+		
 	}
 
 }
