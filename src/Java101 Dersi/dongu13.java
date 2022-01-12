a9595305621df52eda685ca67a9b6c21787089b2
fatmanurcelebi <fatmanuronarir@gmail.com>
2022-01-13 00:03:02 +0300

    .

---
+++ src/Java101 Dersi/dongu13.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class dongu13 {
+
+	public static void main(String[] args) {
+		// 1-100 arasi asal sayiler
+
+		Scanner inp=new Scanner(System.in);
+
+		Boolean asal=true;
+		int n=0;
+		System.out.print("1-100 arasi asal sayilar:  ");
+		
+		for(int i=2;i<=100;i++) {
+			asal=true;
+			for(int k=i-1;k>1;k--) {
+
+				if(i%k==0 ) {
+					asal=false;
+					break;
+				}
+				
+				
+				
+				
+			}
+			
+		
+			
+			
+			
+			if(asal) {
+				
+				System.out.print(i+",");
+			
+			
+			
+			
+		}
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
