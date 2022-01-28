2e3a83013e55abb958f2bccd7214ee8c0f024475
fatmanurcelebi <fatmanuronarir@gmail.com>
2022-01-28 13:56:56 +0300

    Update RecursivePower.java

--- src/Java101 Dersi/RecursivePower.java
+++ src/Java101 Dersi/RecursivePower.java
@@ -1,17 +1,6 @@
 import java.util.Scanner;
 public class RecursivePower {
 
-	
-	static int power (int a,int n) {
-		int result=1;
-	
-	if(n==1) {
-		return a;
-	}else {
-	result=a*power(a,n-1);
-	return result ;
-	}}
-	
 	public static void main(String[] args) {
 		Scanner inp=new Scanner(System.in);
 		System.out.println("Enter the first number: ");
@@ -22,5 +11,16 @@ public class RecursivePower {
 		int result=power(a,n);
 System.out.println(a+"^"+n+" = "+result);	
 	}
+	static int power (int a,int n) {
+		int result=1;
+	
+	if(n==1) {
+		return a;
+	}else {
+	result=a*power(a,n-1);
+	return result ;
+	}}
+	
+	
 
 }
